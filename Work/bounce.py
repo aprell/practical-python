6f214cd8c0030d11bd33e01cd6a239f3c630686e
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-28 21:47:05 +0200

    Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
-# bounce.py
-#
 # Exercise 1.5
+
+def bounce(height):
+    return 3/5 * height
+
+height = 100
+
+for i in range(1, 11):
+    height = bounce(height)
+    print(i, round(height, 4))
