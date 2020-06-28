6d58b5304b5b3f6a3c6ab1e2df264bffaf1263fd
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-28 21:50:16 +0200

    Exercises 1.7-1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,26 @@
-# mortgage.py
-#
-# Exercise 1.7
+# Exercises 1.7-1.11
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 1
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000.0
+
+while principal > payment:
+    principal = principal * (1 + rate/12) - payment
+    total_paid += payment
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    print(month, round(total_paid, 2), round(principal, 2))
+    month += 1
+
+total_paid += principal
+principal = 0
+print(month, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2), 'over', month, 'months')
