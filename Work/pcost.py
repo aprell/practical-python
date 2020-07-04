79cb3991979ff8890c0e30244bca6e1e2541402d
Andreas Prell <andreas.h.prell@gmail.com>
2020-07-04 13:39:33 +0200

    Exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,7 @@
-# Exercises 1.27, 1.30-1.32
+# Exercises 1.27, 1.30-1.33
 
 import csv
-from sys import stderr
+import sys
 
 def portfolio_cost(filename):
     portfolio_price = 0
@@ -18,7 +18,12 @@ def portfolio_cost(filename):
                 print(f"{name:5}: {shares:3} x {price:5.2f} = {purchase_price:8.2f}")
                 portfolio_price += purchase_price
             except ValueError:
-                print("Warning: skipping", line, file=stderr)
+                print("Warning: skipping", line, file=sys.stderr)
     return portfolio_price
 
-print("\n\u2211", portfolio_cost("Data/portfolio.csv"))
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+
+print("\n\u2211", portfolio_cost(filename))
