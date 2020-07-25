245eadd9aa1f26a53609ca2ffc830d3a525ace5c
Andreas Prell <andreas.h.prell@gmail.com>
2020-07-25 14:49:17 +0200

    Exercises 2.9-2.12

--- Work/report.py
+++ Work/report.py
@@ -1,4 +1,5 @@
 # Exercises 2.4-2.7
+# Exercises 2.9-2.12
 
 from copy import deepcopy
 import csv
@@ -39,7 +40,7 @@ def portfolio_cost(portfolio):
     for holding in portfolio:
         name, shares, price = holding["name"], holding["shares"], holding["price"]
         purchase_price = shares * price
-        print(f"{name:5}: {shares:3} x {price:6.2f} = {purchase_price:8.2f}")
+        print(f"{name:5}: {shares:3} x {price:6.2f} = {purchase_price:10,.2f}")
         portfolio_price += purchase_price
     return portfolio_price
 
@@ -49,17 +50,41 @@ def portfolio_value(portfolio, prices):
         holding["price"] = prices[holding["name"]]
     return portfolio_cost(portfolio)
 
+def make_report(portfolio, prices):
+    report = []
+    for holding in portfolio:
+        report.append(
+            {
+                "name": holding["name"],
+                "shares": holding["shares"],
+                "price": prices[holding["name"]],
+                "change": prices[holding["name"]] - holding["price"]
+            }
+        )
+    return report
+
+def print_report(portfolio, prices):
+    headers = ("Name", "Shares", "Price", "Change")
+    hlines = (10 * "-", 10 * "-", 10 * "-", 10 * "-")
+    print("%10s %10s %10s %10s" % headers)
+    print(" ".join(hlines))
+    for line in make_report(portfolio, prices):
+        line["price"] = "$%.2f" % line["price"]
+        print("{name:>10s} {shares:>10d} {price:>10s} {change:>+10.2f}".format_map(line))
+
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     filename = "Data/portfolio.csv"
 
 portfolio = read_portfolio(filename)
-cost = portfolio_cost(portfolio)
-print(f"\n\u2211 {cost:.2f}\n")
+#cost = portfolio_cost(portfolio)
+#print(f"\n\u2211 {cost:,.2f}\n")
 
 prices = read_prices("Data/prices.csv")
-value = portfolio_value(portfolio, prices)
-print(f"\n\u2211 {value:.2f}\n")
+#value = portfolio_value(portfolio, prices)
+#print(f"\n\u2211 {value:,.2f}\n")
 
-print(f"{value:.2f} - {cost:.2f} = {value - cost:.2f}")
+#print(f"{value:,.2f} - {cost:,.2f} = {value - cost:,.2f}")
+
+print_report(portfolio, prices)
