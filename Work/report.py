17873db5d58d8d9070606dcf1f10bd4e8c332376
Andreas Prell <andreas.h.prell@gmail.com>
2020-07-26 12:09:54 +0200

    Exercise 2.20

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # Exercises 2.4-2.7
 # Exercises 2.9-2.12
-# Exercise 2.16
+# Exercises 2.16, 2.20
 
 from copy import deepcopy
 import csv
@@ -36,20 +36,23 @@ def read_portfolio(filename):
                 print(f"Skipping {filename}, line {lineno}:", line, file=sys.stderr)
     return portfolio
 
-def portfolio_cost(portfolio):
-    portfolio_price = 0
-    for holding in portfolio:
-        name, shares, price = holding["name"], holding["shares"], holding["price"]
-        purchase_price = shares * price
-        print(f"{name:5}: {shares:3} x {price:6.2f} = {purchase_price:10,.2f}")
-        portfolio_price += purchase_price
-    return portfolio_price
+def portfolio_cost(portfolio, verbose=True):
+    if verbose:
+        portfolio_price = 0
+        for holding in portfolio:
+            name, shares, price = holding["name"], holding["shares"], holding["price"]
+            purchase_price = shares * price
+            print(f"{name:5}: {shares:3} x {price:6.2f} = {purchase_price:10,.2f}")
+            portfolio_price += purchase_price
+        return portfolio_price
+    else:
+        return sum([holding["shares"] * holding["price"] for holding in portfolio])
 
-def portfolio_value(portfolio, prices):
+def portfolio_value(portfolio, prices, verbose=True):
     portfolio = deepcopy(portfolio)
     for holding in portfolio:
         holding["price"] = prices[holding["name"]]
-    return portfolio_cost(portfolio)
+    return portfolio_cost(portfolio, verbose)
 
 def make_report(portfolio, prices):
     report = []
@@ -78,11 +81,11 @@ else:
     filename = "Data/portfolio.csv"
 
 portfolio = read_portfolio(filename)
-#cost = portfolio_cost(portfolio)
+#cost = portfolio_cost(portfolio, verbose=False)
 #print(f"\n\u2211 {cost:,.2f}\n")
 
 prices = read_prices("Data/prices.csv")
-#value = portfolio_value(portfolio, prices)
+#value = portfolio_value(portfolio, prices, verbose=False)
 #print(f"\n\u2211 {value:,.2f}\n")
 
 #print(f"{value:,.2f} - {cost:,.2f} = {value - cost:,.2f}")
