12de130836a22ee8d94798f4905afb0f30e4376b
Andreas Prell <andreas.h.prell@gmail.com>
2020-07-28 22:12:46 +0200

    Exercises 3.8-3.10

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,8 +1,9 @@
-# Exercises 3.3-3.7
+# Exercises 3.3-3.10
 
 import csv
+import sys
 
-def parse_csv_dicts(filename, delimiter, select=None, types=None):
+def parse_csv_dicts(filename, delimiter, select=None, types=None, silence_errors=False):
     """
     Parse a CSV file into a list of dicts
     """
@@ -23,20 +24,26 @@ def parse_csv_dicts(filename, delimiter, select=None, types=None):
                 if key not in types:
                     types[key] = str
 
-        for line in lines:
-            if not line:
-                continue
-            if columns:
-                line = [line[i] for i in columns]
-            if types:
-                records.append({
-                    key: types[key](line[i]) for i, key in enumerate(keys)
-                })
-            else:
-                records.append(dict(zip(keys, line)))
+        for lineno, line in enumerate(lines, start=1):
+            try:
+                if not line:
+                    continue
+                if columns:
+                    line = [line[i] for i in columns]
+                if types:
+                    records.append({
+                        key: types[key](line[i]) for i, key in enumerate(keys)
+                    })
+                else:
+                    records.append(dict(zip(keys, line)))
+            except ValueError as err:
+                if not silence_errors:
+                    print(f"Skipping {filename}, line {lineno}:", line, file=sys.stderr)
+                    print(f"Reason:", err, file=sys.stderr)
+
     return records
 
-def parse_csv_tuples(filename, delimiter, types=None):
+def parse_csv_tuples(filename, delimiter, types=None, silence_errors=False):
     """
     Parse a CSV file into a list of tuples
     """
@@ -44,23 +51,30 @@ def parse_csv_tuples(filename, delimiter, types=None):
     with open(filename, "rt") as file:
         lines = csv.reader(file, delimiter=delimiter)
 
-        for line in lines:
-            if not line:
-                continue
-            if types:
-                assert len(types) == len(line)
-                line = [convert(val) for convert, val in zip(types, line)]
-            records.append(tuple(line))
+        for lineno, line in enumerate(lines, start=1):
+            try:
+                if not line:
+                    continue
+                if types:
+                    assert len(types) == len(line)
+                    line = [convert(val) for convert, val in zip(types, line)]
+                records.append(tuple(line))
+            except ValueError as err:
+                if not silence_errors:
+                    print(f"Skipping {filename}, line {lineno}:", line, file=sys.stderr)
+                    print(f"Reason:", err, file=sys.stderr)
+
     return records
 
 # The revenge for straying off course...
-def parse_csv(filename, has_headers, delimiter=',', select=None, types=None):
+def parse_csv(filename, has_headers, delimiter=',', select=None, types=None, silence_errors=False):
     if has_headers:
         if types:
             assert type(types) is dict
-        return parse_csv_dicts(filename, delimiter, select, types)
+        return parse_csv_dicts(filename, delimiter, select, types, silence_errors)
     else:
-        assert not select, "select not supported"
+        if select:
+            raise RuntimeError("select requires column names")
         if types:
             assert type(types) is list
-        return parse_csv_tuples(filename, delimiter, types)
+        return parse_csv_tuples(filename, delimiter, types, silence_errors)
