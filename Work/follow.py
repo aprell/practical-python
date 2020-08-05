b8a5f284027d4c2bd8293520d661e2e0be9db7c0
Andreas Prell <andreas.h.prell@gmail.com>
2020-08-05 16:08:31 +0200

    Exercise 6.7

--- Work/follow.py
+++ Work/follow.py
@@ -1,4 +1,4 @@
-# Exercises 6.5, 6.6
+# Exercises 6.5-6.7
 
 import os
 import time
@@ -14,10 +14,14 @@ def follow(filename):
             yield line
 
 if __name__ == "__main__":
+    from report import read_portfolio
+
+    portfolio = read_portfolio("Data/portfolio.csv")
+
     for line in follow("Data/stocklog.csv"):
         fields = line.split(",")
         name = fields[0].strip('"')
         price = float(fields[1])
         change = float(fields[4])
-        if change < 0:
+        if name in portfolio:
             print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
