78d45645d0b8917ac028d0d35dc3f4ee5a80d0dc
Andreas Prell <andreas.h.prell@gmail.com>
2020-08-05 16:03:38 +0200

    Exercise 6.6

--- Work/follow.py
+++ Work/follow.py
@@ -1,19 +1,23 @@
-# Exercise 6.5
+# Exercises 6.5, 6.6
 
 import os
 import time
 
-file = open("Data/stocklog.csv")
-file.seek(0, os.SEEK_END)
+def follow(filename):
+    with open(filename, "rt") as file:
+        file.seek(0, os.SEEK_END)
+        while True:
+            line = file.readline()
+            if not line:
+                time.sleep(0.1)
+                continue
+            yield line
 
-while True:
-    line = file.readline()
-    if not line:
-        time.sleep(0.1)
-        continue
-    fields = line.split(",")
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
+if __name__ == "__main__":
+    for line in follow("Data/stocklog.csv"):
+        fields = line.split(",")
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if change < 0:
+            print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
