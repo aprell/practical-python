155aba4604832d8398e8920f34c7184375d69dbd
Andreas Prell <andreas.h.prell@gmail.com>
2020-08-08 14:27:35 +0200

    Exercises 7.7-7.9

--- Work/stock.py
+++ Work/stock.py
@@ -1,8 +1,15 @@
 # Exercises 4.1, 4.2, 4.9
 # Exercises 5.6-5.8
+# Exercises 7.7-7.9
+
+from typedproperty import String, Integer, Float
 
 class Stock:
-    __slots__ = ("name", "_shares", "price")
+    __slots__ = ("_name", "_shares", "_price")
+
+    name = String("name")
+    shares = Integer("shares")
+    price = Float("price")
 
     def __init__(self, name, shares, price):
         self.name = name
@@ -11,16 +18,6 @@ class Stock:
 
     def __repr__(self):
         return f"Stock({self.name}, {self.shares}, {self.price:.2f})"
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, shares):
-        if type(shares) is not int:
-            raise TypeError("Expected int")
-        self._shares = shares
 
     @property
     def cost(self):
