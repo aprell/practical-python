acf06f439a835c8b6ee001f9d11aa004749a0cac
Andreas Prell <andreas.h.prell@gmail.com>
2020-08-13 19:17:28 +0200

    Exercise 8.1

---
+++ Work/test_stock.py
@@ -0,0 +1,32 @@
+# Exercise 8.1
+
+from stock import Stock
+import unittest
+
+class TestStock(unittest.TestCase):
+    def test_create(self):
+        s = Stock("GOOG", 100, 490.1)
+        self.assertEqual(s.name, "GOOG")
+        self.assertEqual(s.shares, 100)
+        self.assertAlmostEqual(s.price, 490.1)
+
+    def test_cost(self):
+        s = Stock("GOOG", 100, 490.1)
+        self.assertEqual(s.cost, 49010)
+
+    def test_sell(self):
+        s = Stock("GOOG", 100, 490.1)
+        s.sell(50)
+        self.assertEqual(s.shares, 50)
+        s.sell(30)
+        self.assertEqual(s.shares, 20)
+        s.sell(30)
+        self.assertEqual(s.shares, 0)
+
+    def test_shares(self):
+        s = Stock("GOOG", 100, 490.1)
+        with self.assertRaises(TypeError):
+            s.shares = "100"
+
+if __name__ == "__main__":
+    unittest.main()
